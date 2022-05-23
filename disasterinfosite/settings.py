fe217b2f27deeac80d9141f6af958521a7fdfdfe
melinda <melinda@melindaminch.com>
2022-05-23 16:41:46 -0700

    Syntax updates

--- disasterinfosite/settings.py
+++ disasterinfosite/settings.py
@@ -136,6 +136,8 @@ DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Allow database connections to persist
 CONN_MAX_AGE = environ.get('CONN_MAX_AGE') or 0
 
